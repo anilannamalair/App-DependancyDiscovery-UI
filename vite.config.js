26bdef34f6032c42f7a9b7d6327b5e8c456195c9
anilannamalair <anilannamalair@gmail.com>
2025-01-08 18:12:41 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 8085,  // Set the port to 8085
+    host: '0.0.0.0',  // Allow access from external network
+    port: 5173,        // Ensure the correct port is used
   },
 })
+
